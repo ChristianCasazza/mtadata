ab2bf2c53e7fddc0dfc5518269395b9b131f9916
christian <christiancasazza99@gmail.com>
2024-10-17 11:35:07 -0400

    removed files

--- testing/transformation/sql/busiest_specific_times.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    EXTRACT(HOUR FROM transit_timestamp) AS transit_hour, 
-    AVG(ridership) AS average_ridership
-FROM 
-    mta
-GROUP BY 
-    transit_hour
-ORDER BY 
-    average_ridership DESC;
