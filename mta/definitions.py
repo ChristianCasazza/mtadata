e1b5453b55b5a5b52cd6a69240ab321282517723
christiancasazza <christiancasazza99@gmail.com>
2025-02-26 06:21:10 -0800

    large_io_manager

--- mta/definitions.py
+++ mta/definitions.py
@@ -1,51 +1,80 @@
-import dagster
-import warnings
-warnings.filterwarnings("ignore", category=dagster.ExperimentalWarning)
-import os
-from dagster import Definitions, FilesystemIOManager, load_assets_from_modules
-from mta.resources.io_manager_list import *
-from mta.assets.ingestion import mta_assets, weather_assets
-from mta.assets.ingestion.mta_assets import *
-from mta.assets.ingestion.weather_assets import *
-from mta.assets.transformations.duckdb_assets import duckdb_warehouse
+# File: mta/definitions.py
 
+import dagster
+import os
+
+from dagster import Definitions, FilesystemIOManager, load_assets_from_modules
 from dagster_dbt import DbtCliResource
-from mta.assets.dbt_assets import dbt_project_assets  # Include the dbt assets
-from mta.resources.dbt_project import dbt_project 
-from mta.constants import BASE_PATH
+
+# --- Local imports ---
+# IO Managers
+from mta.resources.io_manager_list import *
 from mta.resources.io_managers.polars_parquet_io_manager import PolarsParquetIOManager
 from mta.resources.io_managers.single_file_polars_parquet_io_manager import SingleFilePolarsParquetIOManager
+from mta.resources.io_managers.large_socrata_polars_parquet_io_manager import large_socrata_polars_parquet_io_manager
 
+
+# Socrata resource
 from mta.resources.socrata_resource import SocrataResource
 
-# Load MTA and Weather assets
+# Asset modules
+from mta.assets.ingestion import mta_assets, weather_assets, crime_assets
+from mta.assets.ingestion.mta_assets import *
+from mta.assets.ingestion.weather_assets import *
+from mta.assets.ingestion.crime_assets import *
+from mta.assets.transformations.duckdb_assets import duckdb_warehouse
+from mta.assets.dbt_assets import dbt_project_assets
+from mta.resources.dbt_project import dbt_project
+
+# Constants
+from mta.constants import BASE_PATH, HOURLY_PATH
+
+# --- 1) Load ingestion assets ---
 mta_assets = load_assets_from_modules([mta_assets])
 weather_assets = load_assets_from_modules([weather_assets])
+crime_assets = load_assets_from_modules([crime_assets])
 
-
-# Other assets like DuckDB
+# --- 2) Other transformation assets ---
 other_assets = [duckdb_warehouse]
 
+# --- 3) IO Managers ---
+# Default single-file Polars Parquet IO Manager
 io_manager = SingleFilePolarsParquetIOManager(base_dir=BASE_PATH)
 
-# Step 3: Create the Socrata resource
-socrata = SocrataResource()  # If using default env var for the token
+# Specialized hourly filesystem IO manager
 hourly_filesystem_io_manager = {
     "hourly_mta_io_manager": FilesystemIOManager(base_dir=HOURLY_PATH)
 }
 
+# Large Socrata IO Manager (for large batch-parquet ingestion)
+large_socrata_io_mgr = large_socrata_polars_parquet_io_manager.configured(
+    {"base_dir": BASE_PATH}
+)
 
-# Resource definitions, including the I/O manager for DuckDB, DBT, and Filesystem
+
+# --- 4) Resources ---
 resources = {
-    "dbt": DbtCliResource(project_dir=dbt_project.project_dir),  # Updated DBT resource reference
+    # DBT CLI Resource
+    "dbt": DbtCliResource(project_dir=dbt_project.project_dir),
+
+    # Default IO manager (single file per asset)
     "io_manager": io_manager,
-    **hourly_filesystem_io_manager,  # Hourly Filesystem IO manager
-    "socrata": socrata,
+
+    # Specialized hourly filesystem IO manager
+    **hourly_filesystem_io_manager,
+
+    # Socrata API Resource
+    "socrata": SocrataResource(),
+
+    # Large Socrata IO Manager (for large single-folder parquet batches)
+    "large_socrata_io_manager": large_socrata_io_mgr,
 }
 
+# --- 5) Gather all assets ---
+all_assets = mta_assets + weather_assets + crime_assets + other_assets + [dbt_project_assets]
 
-# Dagster Definitions for assets, resources, and jobs
+# --- 6) Dagster Definitions ---
 defs = Definitions(
-    assets=mta_assets + weather_assets + other_assets + [dbt_project_assets],  # Include all MTA and DBT assets
-    resources=resources
+    assets=all_assets,
+    resources=resources,
 )
